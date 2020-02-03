fc8e898f706bdaa82f73784fe0829a92362a0736
Aditya Prakash <adityaprakash.bobby@gmail.com>
2020-02-03 17:12:47 +0530

    modify script for simple webserver

--- main.tf
+++ main.tf
@@ -3,10 +3,24 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0620d12a9cf777c87"
-    instance_type = "t2.micro"
-    
+    ami                    = "ami-0620d12a9cf777c87"
+    instance_type          = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.terraform-instance.id]
+
+    user_data = file("start-server.sh")
+
     tags = {
         Name = "terraform-example"
+    }
+}
+
+resource "aws_security_group" "terraform-instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
