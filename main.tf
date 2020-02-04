9615b6b689d97cbdac22d728fbb76b586c63378d
Aditya Prakash <adityaprakash.bobby@gmail.com>
2020-02-04 11:55:39 +0530

    add target group and security groups; add listener rules

--- main.tf
+++ main.tf
@@ -39,13 +39,46 @@ resource "aws_autoscaling_group" "webserver" {
     }
 }
 
-# Get VPC and subnet data for use in ASGs
-data "aws_vpc" "default" {
-    default = true
+resource "aws_lb" "webserver" {
+    name = "webserver-elb"
+    internal = false
+    load_balancer_type = "application"
+    security_groups = [aws_security_group.lb.id]
+    subnets = data.aws_subnet_ids.default.ids
 }
 
-data "aws_subnet_ids" "default" {
-    vpc_id = data.aws_vpc.default.id
+resource "aws_lb_listener" "httplistener" {
+    load_balancer_arn = aws_lb.webserver.load_balancer_arn
+    port              = 80
+    protocol          = "HTTP"
+
+    # send 404 response if any requests don't match
+    default_action {
+        type = "fixed-response"
+
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "404: Page not found"
+            status_code  = 404
+        }
+    }
+}
+
+resource "aws_lb_target_group" "webserver-tg" {
+    name     = "webserver-tg-asg"
+    port     = var.server_port
+    protocol = "HTTP"
+    vpc_id   = data.aws_vpc.default.id
+
+    health_check {
+        path                = "/"
+        protocol            = "HTTP"
+        matcher             = "200"
+        internal            = 15
+        timeout             = 5
+        healthy_threshold   = 2
+        unhealthy_threshold = 2
+    }
 }
 
 resource "aws_security_group" "webserver" {
@@ -57,4 +90,31 @@ resource "aws_security_group" "webserver" {
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+resource "aws_security_group" "lb" {
+    name = "terraform-example-lb-sg"
+    
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+# Get VPC and subnet data for use in ASGs
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet_ids" "default" {
+    vpc_id = data.aws_vpc.default.id
 }
